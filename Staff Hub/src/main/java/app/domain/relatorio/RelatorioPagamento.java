58610a899de6279c26374491a13115485ec3095b
Gabriela Vieira Ramos <gabrielavieirar21@gmail.com>
2025-09-21 22:19:47 -0300

    remocao interface

--- Staff Hub/src/main/java/app/domain/relatorio/RelatorioPagamento.java
+++ Staff Hub/src/main/java/app/domain/relatorio/RelatorioPagamento.java
@@ -1,10 +1,9 @@
-// 17) RelatorioPagamento (Refined Abstraction)
 package domain.relatorio;
 
 public class RelatorioPagamento extends Relatorio {
     public RelatorioPagamento(Exporter exporter) { super(exporter); }
+
     protected String montarConteudo() {
-        // mock: normalmente viria do DAO/consulta
-        return "Relatorio de Pagamentos\n- Usuario 7: R$ 3.120,00\n";
+        return "Relatorio de Pagamentos\nUsuario 7; R$ 3120.00\n";
     }
 }
